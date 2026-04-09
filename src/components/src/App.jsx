1a9236b310a63a7713cfba8bc70118f1c5536c11
kellyannjoefield24-source <kellyannjoefield24@gmail.com>
2026-04-09 08:56:25 -0300

    Create App.jsx

---
+++ src/components/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import ProductList from './ProductList';
+import './App.css';
+
+function App() {
+  const [showProductList, setShowProductList] = useState(false);
+
+  const handleGetStartedClick = () => {
+    setShowProductList(true);
+  };
+
+  return (
+    <div className="app-container">
+      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+        <div className="background-image"></div>
+        <div className="content">
+         <div className="landing_content">
+            <h1>Paradise Nursery</h1>
+            <div className="divider"></div>
+            <p>Where Greenery Meets Serenity</p>
+            <button className="get-started-button" onClick={handleGetStartedClick}>
+              Get Started
+            </button>
+          </div>
+          <div className="aboutus_container">
+            <p className="about_us_description">Welcome to Paradise Nursery, where we bring nature closer to you.</p>
+          </div>
+        </div>
+      </div>
+      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+        <ProductList />
+      </div>
+    </div>
+  );
+}
+
+export default App;
